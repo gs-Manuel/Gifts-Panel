a97adfc0915b33cee8d692df21eff486528b88b2
Manuel <gs_manuel@outlook.es>
2023-07-17 11:14:45 +0200

    HolaMundo usando JavaFX

---
+++ PanelRegalos/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module gonzalezsantos.manuel.panelregalos {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens gonzalezsantos.manuel.panelregalos to javafx.fxml;
+    exports gonzalezsantos.manuel.panelregalos;
+}
